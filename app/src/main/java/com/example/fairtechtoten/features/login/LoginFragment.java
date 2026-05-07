d5525a056dae1b01764c972ccdc5ec7987f76316
kevin-vinagre <lkevinvinagre@hotmail.com>
2026-05-07 09:25:40 -0400

    action and arguments introduction

--- app/src/main/java/com/example/fairtechtoten/features/login/LoginFragment.java
+++ app/src/main/java/com/example/fairtechtoten/features/login/LoginFragment.java
@@ -5,26 +5,66 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.fairtechtoten.R;
+import com.example.fairtechtoten.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends Fragment {
 
+    private FragmentLoginBinding binding;
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
+        super.onViewCreated(view, savedInstanceState);
+
+        setupListener();
+
+
+    }
+
+    private void setupListener() {
+
+        binding.loginBtn.setOnClickListener(v -> {
+            String email = binding.emailTb.getText().toString();
+            String password = binding.passwordTb.getText().toString();
+            if(email.equals("admin") && password.equals("admin")) {
+
+                Bundle bundle = new Bundle();
+                bundle.putString("email", email);
+
+                Navigation.findNavController(v)
+                        .navigate(R.id.action_login_to_home, bundle);
+
+            } else {
+
+                Toast.makeText(requireContext(),
+                        "Email ou senha incorretos",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null; // Evitar memory leaks
     }
 }
